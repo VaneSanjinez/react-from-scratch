5955190dae6896727161cb763e105997ef97f41a
Vanessa Sanjinez <vanessa.sanjinez@truextend.com>
2019-01-29 14:43:42 -0400

    Add Footer and Header

---
+++ src/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Title from "./Title";
+
+export default class Header extends React.Component {
+  render() {
+    console.log(this.props);
+    //return <header>Header</header>;
+    return (
+      <div>
+        <Title title={this.props.title} />
+        <input />
+      </div>
+    );
+  }
+}
